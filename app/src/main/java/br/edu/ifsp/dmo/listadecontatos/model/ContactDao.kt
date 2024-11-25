02d05fa0b0efb75ae0d3750d7f7387b94a05f93a
guilhermefragiacomo <guilhermefragiacomo1@gmail.com>
2024-11-24 22:11:11 -0300

    base project created

---
+++ app/src/main/java/br/edu/ifsp/dmo/listadecontatos/model/ContactDao.kt
@@ -0,0 +1,14 @@
+package br.edu.ifsp.dmo.listadecontatos.model
+
+import br.edu.ifsp.listadecontatos.model.Contact
+import java.util.LinkedList;
+
+object ContactDao {
+    private val dataset = LinkedList<Contact>()
+    fun insert(contact: Contact) {
+        dataset.add(Contact(contact.name, contact.phone))
+    }
+    fun findAll(): List<Contact> {
+        return ArrayList<Contact>(dataset)
+    }
+}
